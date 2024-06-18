c35bc758919f72ec09614fb4ae01bee4bb66cf9e
mattmorvai <matt.morvai@100starlings.com>
2024-06-18 19:20:43 +0200

    Change data source on service section

--- app/sections/ServicesSection.tsx
+++ app/sections/ServicesSection.tsx
@@ -4,72 +4,7 @@ import { ArrowLeftIcon, ArrowRightIcon, ChevronLeftIcon, ChevronRightIcon } from
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-
-const services = [
-	{
-		name: "Web Development",
-		slug: "web-development",
-		description:
-			"From simple landing pages to complex web applications, we build custom solutions that are tailored to your unique business needs.",
-		imageUrl:
-			"https://images.unsplash.com/photo-1625993254744-2a0a2e8eb836?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-	},
-	{
-		name: "Full Project Delivey",
-		slug: "full-project-delivery",
-		description:
-			"You will get a dedicated team to take care of the full product delivery. We design, we code, and we ship your application.",
-		imageUrl:
-			"https://images.unsplash.com/photo-1625993254657-2ff0a77a3095?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-	},
-	{
-		name: "App Development",
-		slug: "app-development",
-		description: "From iOS to Android, we build mobile applications that are tailored to your unique business needs.",
-		imageUrl:
-			"https://images.unsplash.com/photo-1625993254667-a6b8dc27e531?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-	},
-	{
-		name: "Team Extension",
-		slug: "team-extension",
-		description:
-			"Gain competitive advantige by leveraging a specialized team tailored to meet your unique business requirements. We provide our best developers, consultants who work as part of your team.",
-		imageUrl:
-			"https://images.unsplash.com/photo-1603609413589-eb8ec03ff88b?q=80&w=3271&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-	},
-	{
-		name: "MVP for Startups",
-		slug: "mvp-for-startups",
-		description:
-			"Using our startup experience, we design and build your MVP so you can test your business concept with a real product.",
-		imageUrl:
-			"https://images.unsplash.com/photo-1596387451750-f7bfb51461ef?q=80&w=3271&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-	},
-	{
-		name: "Consulting",
-		slug: "consulting",
-		description:
-			"We understand technology and development, enabling your business to focus on its core strengths. Whether it's selecting the right platform, programming language, or enhancing your existing website, our consultants are here to help.",
-		imageUrl:
-			"https://images.unsplash.com/photo-1598770220555-c835b6c7b11f?q=80&w=3271&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-	},
-	{
-		name: "DevOps",
-		slug: "devops",
-		description:
-			"Our experts excel in implementing CI/CD, infrastructure as code, and cloud-native practices. We take a holistic approach to software delivery, addressing security, automation, compliance, QA, and more for a comprehensive market-ready solution.",
-		imageUrl:
-			"https://images.unsplash.com/photo-1597557316244-a6cafb23b280?q=80&w=3271&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-	},
-	{
-		name: "UX/UI Design",
-		slug: "ux-ui-design",
-		description:
-			"We help you learn more about your users and explore possible competitors and discover market gaps. We completely design your product. Our design team creates code-ready design, speeding up the technological implementation.",
-		imageUrl:
-			"https://images.unsplash.com/photo-1605423357105-1b891069e72b?q=80&w=3271&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-	},
-];
+import services from "@/data/offeredServices.json";
 
 export default function ServicesSection() {
 	//  scroll the services section left and right by 300px when the left and right arrows are clicked
