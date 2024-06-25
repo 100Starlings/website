de1792d069a075576ecb246c21e4880e1addcc3d
mattmorvai <matt.morvai@100starlings.com>
2024-06-25 14:48:06 +0200

    Add clients section layout

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,14 @@ import ContactSection from "./sections/ContactSection";
 import PricingSection from "./sections/PricingSection";
 import Link from "next/link";
 import ProductSection from "./sections/ProductSection";
+import ClientsSection from "./sections/ClientsSection";
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen w-full flex-col items-center justify-between overflow-x-hidden">
 			<HeroSection />
 			<ServicesSection />
+			<ClientsSection />
 			<AboutUsSection />
 			<ProjectsSection />
 			<ProductSection />
