4392f89616bd69a53eb4028941f76dff9a91613b
mattmorvai <matt.morvai@100starlings.com>
2024-06-25 15:03:00 +0200

    Adjust image sizes of clients

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,16 @@ import ProjectsSection from "./sections/ProjectsSection";
 import AboutUsSection from "./sections/AboutUsSection";
 import OurTeamSection from "./sections/OurTeamSection";
 import ContactSection from "./sections/ContactSection";
-import ClientsSection from "./sections/ClientsSection"
+import ClientsSection from "./sections/ClientsSection";
 import PricingSection from "./sections/PricingSection";
 import Link from "next/link";
 import ProductSection from "./sections/ProductSection";
-import ClientsSection from "./sections/ClientsSection";
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen w-full flex-col items-center justify-between overflow-x-hidden">
 			<HeroSection />
-			<ClientsSection />
+			{/* <ClientsSection /> */}
 			<ServicesSection />
 			<ClientsSection />
 			<AboutUsSection />
--- app/sections/ClientsSection.tsx
+++ app/sections/ClientsSection.tsx
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-export default function ClientsSection() {
-  return (
-    <>
-      <div className="flex w-full max-w-7xl justify-between items-center">
-        <p className="header">
-          Borrow our experience
-        </p>
-      </div>
-      <div className="flex w-full max-w-7xl justify-between items-center">
-        <p className="z-10 my-3 text-sm lg:text-md max-w-lg font-light">
-          Clients we have worked with
-        </p>
-      </div>
-      <div className="flex items-center justify-center mb-[6rem]">
-        <div className="flex flex-wrap justify-center items-center px-4">
-          <a href="https://www.yodel.co.uk" target="_blank" rel="noopener noreferrer">
-            <img src="/images/clients/1/Yodel.svg" alt="yodel" className="max-w-xs mx-4 my-2 w-20 object-contain"/>
-          </a>
-          <a href="https://tuskercars.com/" target="_blank" rel="noopener noreferrer">
-            <img src="/images/clients/1/Tusker.svg" alt="tusker" className="max-w-xs mx-4 my-2 w-20 object-contain"/>
-          </a>
-          <a href="https://www.bear.app" target="_blank" rel="noopener noreferrer">
-            <img src="/images/clients/1/Bear.png" alt="bear" className="max-w-xs mx-4 my-2 w-16 object-contain"/>
-          </a>
-          <a href="https://www.learnamp.com" target="_blank" rel="noopener noreferrer">
-            <img src="/images/clients/1/Learnamp.svg" alt="learnamp" className="max-w-xs mx-4 my-2 w-32 object-contain"/>
-          </a>
-          <a href="https://www.streetbank.com" target="_blank" rel="noopener noreferrer">
-            <img src="/images/clients/1/Streetbank.png" alt="streetbank" className="max-w-xs mx-4 my-4 w-28 object-contain"/>
-          </a>
-          <a href="https://www.postshift.com" target="_blank" rel="noopener noreferrer">
-            <img src="/images/clients/1/PostShift.svg" alt="post shift" className="max-w-xs mx-4 my-4 w-24 object-contain"/>
-          </a>
-          <a href="https://field.io/" target="_blank" rel="noopener noreferrer">
-            <img src="/images/clients/1/Field.svg" alt="field" className="max-w-xs mx-4 my-4 w-28 object-contain"/>
-          </a>
-          <a href="https://www.abzed.com" target="_blank" rel="noopener noreferrer">
-            <img src="/images/clients/1/Abzed.svg" alt="abzed" className="max-w-xs mx-4 my-4 w-32 object-contain"/>
-          </a>
-          <a href="https://www.sensata.com/" target="_blank" rel="noopener noreferrer">
-            <img src="/images/clients/1/Sensata.png" alt="field" className="max-w-xs mx-4 my-4 w-20 object-contain"/>
-          </a>
-        </div>
-      </div>
-    </>
-  );
-=======
 import React from "react";
 
 const clients = [
@@ -109,14 +61,13 @@ export default function ClientsSection() {
 	return (
 		<section className="flex flex-col w-full max-w-7xl justify-between items-start gap-[2rem]">
 			<p className="header">Clients we have worked with</p>
-			<div className="flex flex-row flex-wrap items-center justify-center gap-[1rem] lg:justify-start">
+			<div className="flex flex-row flex-wrap lg:flex-nowrap items-center justify-center gap-[3rem] lg:justify-start">
 				{clients.map((client, index) => (
 					<a href={client.websiteUrl} target="_blank" rel="noopener noreferrer">
-						<img src={client.imageURL} alt={client.altText} className="h-auto w-auto object-contain" />
+						<img src={client.imageURL} alt={client.altText} className="w-[4rem] lg:w-auto object-contain" />
 					</a>
 				))}
 			</div>
 		</section>
 	);
->>>>>>> main
 }
