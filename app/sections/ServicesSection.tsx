775c9e5dd859b182a0aa85d1ee2c83ab1ea38604
mattmorvai <matt.morvai@100starlings.com>
2024-07-23 17:16:55 +0200

    Clean up services section

--- app/sections/ServicesSection.tsx
+++ app/sections/ServicesSection.tsx
@@ -6,18 +6,6 @@ import Link from "next/link";
 import React from "react";
 import services from "@/data/offeredServices.json";
 
-const colors = ["#03A79D", "#1C75BC"];
-
-const getRandomGradient = () => {
-	const getRandomColor = () => colors[Math.floor(Math.random() * colors.length)];
-
-	const angle = Math.floor(Math.random() * 360);
-	const color1 = getRandomColor();
-	const color2 = getRandomColor();
-
-	return `linear-gradient(${angle}deg, ${color1}, ${color2})`;
-};
-
 export default function ServicesSection() {
 	const servicesRef = React.useRef<HTMLDivElement>(null);
 
@@ -47,23 +35,14 @@ export default function ServicesSection() {
 			</div>
 			<div
 				ref={servicesRef}
-				// style={{ paddingInline: `max(0rem, calc((100vw - 83.5rem) / 2))` }}
 				className="flex lg:grid grid-cols-4 gap-4 lg:gap-8 w-full overflow-x-scroll snap-x snap-center snap-mandatory"
 			>
 				{services.map((feature) => (
 					<Link
 						href={`/services/${feature.slug}`}
 						key={feature.name}
-						// style={{ background: getRandomGradient() }}
 						className="snap-center rounded-xl gallery-photo overflow-scroll min-w-[20rem] lg:min-w-full h-auto bg-[--card-bg] hover:backdrop-blur-4xl p-0 flex flex-col gap-3"
 					>
-						{/* <Image
-								src={feature.imageUrl}
-								alt="hero"
-								fill
-								style={{ objectFit: "cover", zIndex: "0" }}
-								className="image opacity-0"
-							/> */}
 						<div className="w-full h-full backdrop-blur-none p-6 z-0 flex flex-col gap-2 items-start text-left">
 							<CheckCircleIcon className="text-green h-8" />
 							<p className="font-semibold text-xl text-white/100">{feature.name}</p>
