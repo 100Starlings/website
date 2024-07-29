83fc3439ac7c791d23b19470c05c1f655403a97f
Luísa Kinas <luisa.kinas@gmail.com>
2024-07-29 13:04:36 -0300

    add images and copy

--- app/sections/ContactSection.tsx
+++ app/sections/ContactSection.tsx
@@ -1,25 +1,18 @@
 import { BuildingOffice2Icon } from "@heroicons/react/20/solid";
-import Link from "next/link";
 import { EnvelopeIcon, IdentificationIcon } from "@heroicons/react/24/outline";
 import EmailContactForm from "../components/EmailContactForm";
 import Image from "next/image";
+import BackgroundImage from "../../public/images/background/texture-contact.jpeg"
 
 export default function ContactSection() {
 	return (
-		// <section
-		// 	id="contact"
-		// 	className="w-full max-w-screen max-w-7xl lg:flex card gap-6 p-[8rem] lg:flex-col items-center justify-center snap-start"
-		// >
-		// 	<p className="header mb-4 text-center mx-auto">Let&apos;s solve YOUR challenges</p>
-		// 	<button className="button hidden xl:flex mx-auto">Let&apos;s talk</button>
-		// </section>
 		<section id="contact" className="flex w-full max-w-7xl card h-auto flex-col">
 			<div className="flex flex-col lg:flex-row min-h-[40rem]">
 				<div className="flex gap-6 flex-col w-full min-h-[100%] p-8 lg:p-[4rem] justify-center my-auto">
 					<p className="header"> Let&apos;s solve your challenges</p>
 					<p className="text mb-10">
-						We are a group of talented individuals from different delivery backgrounds. Through our collective
-						experience
+					Connect with us and explore how we can collaborate. <br />
+					One of our team members will be in touch to discuss your needs and how we can help.
 					</p>
 					<div className="text-[--text-darker]">
 						<div className="flex gap-4 items-center mb-[1.25rem]">
@@ -55,7 +48,7 @@ export default function ContactSection() {
 				<div className="w-full min-w-[50%] relative flex flex-col justify-center overflow-hidden rounded-xl">
 					<div className="absolute w-full h-full rounded-xl z-0 bg-black">
 						<Image
-							src="https://images.unsplash.com/photo-1631477076114-9123f721b9dc?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+							src={BackgroundImage}
 							alt="hero"
 							fill
 							style={{ objectFit: "cover", zIndex: "0" }}
--- app/sections/HeroSection.tsx
+++ app/sections/HeroSection.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import BackgroundImage from "../../public/images/background/texture-hero.jpeg"
 
 export default function HeroSection() {
 	return (
 		<section
 			id="/"
-			className="flex relative mb-[6rem] w-full flex-col card items-center justify-center min-h-[72svh] rounded-md overflow-hidden"
+			className="relative flex flex-col items-center justify-center w-full min-h-[72svh] mb-[6rem] overflow-hidden rounded-md card"
 		>
 			<Image
-				src="https://images.unsplash.com/photo-1641912076734-626a72927483?q=80&w=2855&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+				src={BackgroundImage}
 				layout="fill"
 				objectFit="cover"
 				objectPosition="center"
@@ -17,23 +18,19 @@ export default function HeroSection() {
 				loading="eager"
 				className="image grayscale-100"
 			/>
-			<div className="flex text-white flex-col items-center w-full justify-center flex-grow  backdrop-blur-none bg-black/30">
-				<div className="z-10 flex items-center text-center text-[2rem] md:text-[4rem] lg:text-[4rem] tracking-[0.1rem] leading-tight">
-					<h1 className="font-semibold">CREATING </h1>
-					<h2 className="font-semibold ml-2 xl:ml-4">custom</h2>
+			<div className="flex flex-col items-center justify-center w-full flex-grow text-white bg-black/30 backdrop-blur-none">
+				<div className="z-10 flex items-center text-center text-[1.5rem] lg:text-[4rem] tracking-[0.1rem] leading-tight">
+					<h1 className="">CRAFTING CUSTOM</h1>
 				</div>
-				<div className="z-10 text-[1.9rem] md:text-[3rem] lg:text-[3.5rem] flex items-baseline tracking-[0.1rem] leading-tight">
-					<h2 className="mr-2 xl:mr-4 font-semibold">digital</h2>
-					<h1 className="font-semibold">SOLUTIONS </h1>
+				<div className="z-10 flex text-center text-[1.8rem] lg:text-[4rem] tracking-[0.1rem] leading-tight">
+					<h1 className="font-bold">DIGITAL SOLUTIONS</h1>
 				</div>
-				<p className="z-10 mt-6 text-sm px-10 lg:px-0 lg:text-md text-center max-w-lg font-light">
-					We are a cooperative of like minded professionals skilled at solving problems and creating excellent products
-					for today&apos;s digital world.
+				<p className="z-10 mt-3 px-10 text-center text-sm lg:px-0 lg:text-md max-w-lg font-light">
+					We are a dynamic team of technical experts, dedicated to transforming business challenges into efficient custom digital solutions.
 				</p>
 
 				<Link href="/#services" className="z-10 flex gap-6 mt-8">
-					{/* <button className="button">Contact</button> */}
-					<button className="button ">Services</button>
+					<button className="button">Services</button>
 				</Link>
 			</div>
 		</section>
