a02bed8252d886a12b7bf17689411bd82fe62b03
mattmorvai <matt.morvai@100starlings.com>
2024-08-05 16:13:46 +0200

    Add lists to service pages

--- app/components/SpecialistCard.tsx
+++ app/components/SpecialistCard.tsx
@@ -2,18 +2,18 @@ import Image from "next/image";
 import { useRouter } from "next/navigation"; // Import useRouter for navigation
 import React from "react";
 
-export default function SpecialistCard(secialistdata: any) {
+export default function Specialistx(specialist: any) {
 	const router = useRouter(); // Initialize useRouter
 
 	const handleNavigation = () => {
-		router.push(secialistdata.data.calendar || "/#contact");
+		router.push(specialist.data.calendar || "/#contact");
 	};
 
 	return (
-		<div className="flex">
+		<div className="flex w-full">
 			<div className="relative w-full min-w-[50%] min-h-[40svh]">
 				<Image
-					src={secialistdata.data.image_url || ""}
+					src={specialist.data.image_url || ""}
 					className="rounded-xl"
 					fill
 					alt="Image of the team"
@@ -21,8 +21,9 @@ export default function SpecialistCard(secialistdata: any) {
 				/>
 			</div>
 			<div className="flex gap-6 flex-col w-full h-full p-8 lg:p-[4rem] justify-center my-auto">
-				<p className="header">Meet {secialistdata.data.first_name},</p>
-				<p className="text">{secialistdata.data.description}</p>
+				<p className="header">Meet {specialist.data.first_name},</p>
+				<p className="text">{specialist.description}</p>
+
 				<div>
 					<p className="text">Solve your problems with 1-1 guidance</p>
 					<p className="font-light mb-6">Schedule a 30min call with our specialist</p>
