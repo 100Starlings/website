07fea4ab620c544f50ef9383c49eeedf2415b50b
mattmorvai <matt.morvai@100starlings.com>
2024-08-16 12:27:31 +0200

    Remove unused parag

--- app/sections/AboutUsSection.tsx
+++ app/sections/AboutUsSection.tsx
@@ -30,24 +30,6 @@ export default function AboutUsSection() {
 			<div className="relative w-full min-w-[50%] min-h-[40svh] overflow-hidden rounded-xl">
 				<Image src={GroupImage} className="rounded-xl" alt="Image of the team" fill style={{ objectFit: "cover" }} />
 			</div>
-
-			<div id="team-description" className="flex gap-6 flex-col w-full h-full p-8 sm:p-16 justify-center my-auto">
-				<p className="header font-extrabold">Get to know us</p>
-				<p className="text-md font-light text-slate-600 dark:text-white leading-loose">
-					Our journey began in 2015, primarily with custom Ruby on Rails development, and we have since expanded to
-					provide end-to-end digital solutions.
-				</p>
-
-				<p className="text-md font-light text-slate-600 dark:text-white leading-loose">
-					Some of our experience includes delivering mobile applications, building ETL data pipelines with Elixir and
-					AWS, conducting thorough UX/UI research and providing strategic project guidance.
-				</p>
-
-				<p className="text-md font-light text-slate-600 dark:text-white leading-loose">
-					Operating from the UK, Hungary and Brazil, we align our working hours with UK time zones. Known for our
-					reliability, we've often served as a trusted partner within integrated teams.
-				</p>
-			</div>
 		</section>
 	);
 }
